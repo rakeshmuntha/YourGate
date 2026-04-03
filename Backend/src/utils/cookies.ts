1aab110813e3747cf4a9856bb1a3eb0584a58e76
Rakesh <rakeshmuntha27@gmail.com>
2026-04-03 22:30:04 +0530

    fixed render bug

--- Backend/src/utils/cookies.ts
+++ Backend/src/utils/cookies.ts
@@ -7,7 +7,7 @@ export const setTokenCookies = (res: Response, accessToken: string, refreshToken
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? 'strict' : 'lax',
+    sameSite: isProduction ? 'none' : 'lax',
     maxAge: 15 * 60 * 1000, // 15 minutes
     path: '/',
   });
@@ -15,7 +15,7 @@ export const setTokenCookies = (res: Response, accessToken: string, refreshToken
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? 'strict' : 'lax',
+    sameSite: isProduction ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: '/',
   });
